52f689fa9068dff9e725462bf21ea9fd67f9efc6
Nurzhan <12farit21@gmail.com>
2025-05-24 04:23:22 +0500

    unit tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Task
 
-# Create your tests here.
+class TaskTests(TestCase):
+    def setUp(self):
+        self.task = Task.objects.create(
+            title="Test Task",
+            description="This is a test task"
+        )
+
+    def test_task_creation(self):
+        self.assertEqual(self.task.title, "Test Task")
+        self.assertEqual(self.task.description, "This is a test task")
+
+    def test_task_list_view(self):
+        response = self.client.get(reverse('task_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Task")
+
+    def test_task_create_view(self):
+        response = self.client.post(reverse('task_create'), {
+            'title': 'New Task',
+            'description': 'New task description'
+        })
+        self.assertEqual(response.status_code, 302)  # Ожидаем редирект после создания
+        self.assertEqual(Task.objects.count(), 2)
